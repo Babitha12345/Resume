5a5313e2073bbadd045dfb18afc814f2453b5b5d
Babitha12345 <78953253+Babitha12345@users.noreply.github.com>
2022-09-02 14:43:43 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditComponent } from './components/edit/edit.component';
+import { MainComponent } from './components/main/main.component';
+import { TestComponent } from './components/test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: EditComponent },
+  {path: 'main' , component: MainComponent},
+  {path: 'edit' ,  component: EditComponent  },
+  {path: 'test', component: TestComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
